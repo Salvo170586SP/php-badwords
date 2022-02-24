f320612c54245adad674e1c6de50511a14d2e6ae
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-24 15:39:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+$text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque ex vero quibusdam maiores natus';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php echo $text ?> </p>
+</body>
+</html>
