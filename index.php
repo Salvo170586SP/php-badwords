92206de9bec1fd4a18caca1cb9fc2d2d4e93d246
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-25 18:41:29 +0100

    added html and strlen

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
 //creo variabile della frase
 $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque ex vero quibusdam maiores natus";
 
-//stampo la frase e la sua lunghezza
-var_dump($text);
-
 //creo variabile con la chiava 'parola'
 $word = $_GET ? $_GET['parola'] : '';
 
 //creo variabile della frase con la parola da sostituire
 $text_censured = str_replace( $word, '***', $text);
-
-//stampo la frase con la parola censurata
-echo $text_censured;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php echo $text ?></p>
+    <p> il testo è grande <?php echo strlen($text) ?> caratteri </p>
+    <hr>
+    <p><?php echo $text_censured ?></p>
+</body>
+</html>
